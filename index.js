e278711a95ec6f99f0f8d6ad14b609f743f68d17
Causa <causadev@proton.me>
2023-09-03 08:59:22 -0500

    New cool changes :)

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const rockBtn = document.querySelector("#rockBtn");
 const paperBtn = document.querySelector("#paperBtn");
 const scissorsBtn = document.querySelector("#scissorsBtn");
 const result = document.querySelector("#result");
+const result1 = document.querySelector("#result1");
+const result2 = document.querySelector("#result2");
+let userInput = "joel";
+let playerWins = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 rockBtn.addEventListener("click", () => playRound("rock"));
 paperBtn.addEventListener("click", () => playRound("paper"));
@@ -23,8 +29,17 @@ function playRound(playerSelection) {
     (playerSelection === "scissors" && computerSelection === "rock")
   ) {
     result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore++;
+    result2.textContent = `Computer: ${computerScore}`;
   } else {
     result.textContent = `You Win! ${computerSelection} beats ${playerSelection}`;
+    playerScore++;
   }
+
+  updateScore();
 }
 
+function updateScore() {
+  result1.textContent = `${userInput}: ${playerScore}`;
+  result2.textContent = `Computer: ${computerScore}`;
+}
