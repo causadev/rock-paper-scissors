8bd231258fc68e95e43ac8e51933796efd14a0ec
Causa <causadev@proton.me>
2023-09-03 09:30:06 -0500

    More cool changes :)

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const scissorsBtn = document.querySelector("#scissorsBtn");
 const result = document.querySelector("#result");
 const result1 = document.querySelector("#result1");
 const result2 = document.querySelector("#result2");
-let userInput = "joel";
+const result3 = document.querySelector("#result3");
+const restartBtn = document.querySelector("#restartBtn");
+let userInput = prompt("Enter your username:").toLowerCase();
 let playerWins = 0;
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
+const rounds = 5;
 
 rockBtn.addEventListener("click", () => playRound("rock"));
 paperBtn.addEventListener("click", () => playRound("paper"));
@@ -20,20 +24,28 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection) {
-  const computerSelection = getComputerChoice();
-  if (playerSelection === computerSelection) {
-    result.textContent = `It's a draw! ${playerSelection} cant't beat ${computerSelection}`;
-  } else if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
-  ) {
-    result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
-    computerScore++;
-    result2.textContent = `Computer: ${computerScore}`;
-  } else {
-    result.textContent = `You Win! ${computerSelection} beats ${playerSelection}`;
-    playerScore++;
+  if (round <= rounds) {
+    const computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection) {
+      result.textContent = `It's a draw! ${playerSelection} can't beat ${computerSelection}`;
+    } else if (
+      (playerSelection === "rock" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "scissors") ||
+      (playerSelection === "scissors" && computerSelection === "rock")
+    ) {
+      result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+      computerScore++;
+    } else {
+      result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+      playerScore++;
+    }
+
+    round++;
+    updateScore();
+
+    if (round > rounds) {
+      displayFinalScores();
+    }
   }
 
   updateScore();
@@ -41,5 +53,22 @@ function playRound(playerSelection) {
 
 function updateScore() {
   result1.textContent = `${userInput}: ${playerScore}`;
-  result2.textContent = `Computer: ${computerScore}`;
+  result2.textContent = `computer: ${computerScore}`;
 }
+
+function displayFinalScores() {
+  result3.textContent = `Game Over! Final Scores - ${userInput}: ${playerScore}, computer: ${computerScore}`;
+}
+
+restartBtn.addEventListener("click", () => {
+  round = 0;
+  playerScore = 0;
+  computerScore = 0;
+  result.textContent = "";
+  result1.textContent = "";
+  result2.textContent = "";
+  result3.textContent = "";
+  setTimeout(() => {
+    userInput = prompt("Enter your username:").toLowerCase();
+  }, 100);
+});
